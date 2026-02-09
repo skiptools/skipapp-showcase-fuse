a57867b09a37f0118dd9ffed43af2aae57da7e70
Abe White <abewhite@mac.com>
2026-02-08 20:54:30 -0600

    Exercise fix to search field padding

--- Sources/ShowcaseFuse/SearchablePlayground.swift
+++ Sources/ShowcaseFuse/SearchablePlayground.swift
@@ -135,6 +135,7 @@ struct GridSearchablePlayground: View {
                     .frame(height: 80)
                 }
             }
+            .padding(.horizontal, 40)
         }
         .searchable(text: $searchText)
     }
